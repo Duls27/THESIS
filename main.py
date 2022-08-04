b29568c9bf05d046735d72a9a7864c7415200d8b
Duls27 <Simone.dopti@gmail.com>
2022-08-04 12:00:30 +0200

    Divisione corretta in due immagini

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ files=[x for x in list_of_files if x.endswith(".png") or x.endswith(".jpg")]
 
 #for file in files:
     #rootate_functions.detect_optics(path=str(path+file), path_ooutput=ouput_path, file_name=file)
-    #rootate_functions.canny_houge_rotation(img=str(path+file), ouput_path=ouput_path, file_name=file)
+ #   rootate_functions.canny_houge_rotation(img=str(path+file), ouput_path=ouput_path, file_name=file)
 
-#
-rootate_functions.find_sudoku(img=str(path+"9001.png"))
+#rootate_functions.find_sudoku(img=str(path+"9001.png"))
+
+#rootate_functions.cca_analisys(img=str(path+"9001.png"))
+
+
+rootate_functions.test(img=str(path+"9001.png"))
--- rootate_functions.py
+++ rootate_functions.py
@@ -1,10 +1,9 @@
-import numpy as np
-import cv2
-import math
 import pytesseract
-from scipy.ndimage import rotate as Rotate
 from scipy import ndimage
-import argparse
+import numpy as np
+from PIL import Image
+import math
+import cv2
 
 def detect_optics (path: str, path_ooutput: str, file_name: str):
     pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
@@ -110,22 +109,8 @@ def canny_houge_rotation(img: str, ouput_path: str, file_name: str):
 def find_sudoku(img: str):
     image = cv2.imread(img)
 
-    cv2.namedWindow("Before", cv2.WINDOW_NORMAL)
-    cv2.imshow("Before", image)
-    key = cv2.waitKey(0)
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    cv2.namedWindow("gray", cv2.WINDOW_NORMAL)
-    cv2.imshow("gray", gray)
-    key = cv2.waitKey(0)
-
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    cv2.namedWindow("blur", cv2.WINDOW_NORMAL)
-    cv2.imshow("blur", blur)
-    key = cv2.waitKey(0)
-
     thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
     cv2.namedWindow("thresh", cv2.WINDOW_NORMAL)
@@ -150,20 +135,18 @@ def find_sudoku(img: str):
 
     mask = np.zeros((gray.shape), np.uint8)
     cv2.drawContours(mask, [best_cnt], 0, 255, -1)
-    cv2.drawContours(mask, [best_cnt], 0, 0, 2)
+    #cv2.drawContours(mask, [best_cnt], 0, 0, 2)
 
-
-    """
     out = np.zeros_like(gray)
     out[mask == 255] = gray[mask == 255]
 
 
     blur = cv2.GaussianBlur(out, (5, 5), 0)
 
-    cv2.namedWindow("blur", cv2.WINDOW_NORMAL)
-    cv2.imshow("blur", blur)
+    cv2.namedWindow("blurred_mask", cv2.WINDOW_NORMAL)
+    cv2.imshow("blurred_mask", out)
     key = cv2.waitKey(0)
-
+    """
 
     thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
@@ -185,4 +168,127 @@ def find_sudoku(img: str):
     key = cv2.waitKey(0)
     """
     cv2.destroyAllWindows()
+
+
+def cca_analisys (img: str):
+    # Loading the image
+    img = cv2.imread(img)
+
+    # preprocess the image
+    gray_img = cv2.cvtColor(img,
+                            cv2.COLOR_BGR2GRAY)
+
+    # Applying 7x7 Gaussian Blur
+    blurred = cv2.GaussianBlur(gray_img, (7, 7), 0)
+
+    # Applying threshold
+    threshold = cv2.threshold(blurred, 0, 255,
+                              cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    # Apply the Component analysis function
+    analysis = cv2.connectedComponentsWithStats(threshold,
+                                                4,
+                                                cv2.CV_32S)
+    (totalLabels, label_ids, values, centroid) = analysis
+
+    # Initialize a new image to
+    # store all the output components
+    output = np.zeros(gray_img.shape, dtype="uint8")
+
+    # Loop through each component
+    for i in range(1, totalLabels):
+
+        # Area of the component
+        area = values[i, cv2.CC_STAT_AREA]
+
+        if (area > 140) and (area < 400):
+            # Create a new image for bounding boxes
+            new_img = img.copy()
+
+            # Now extract the coordinate points
+            x1 = values[i, cv2.CC_STAT_LEFT]
+            y1 = values[i, cv2.CC_STAT_TOP]
+            w = values[i, cv2.CC_STAT_WIDTH]
+            h = values[i, cv2.CC_STAT_HEIGHT]
+
+            # Coordinate of the bounding box
+            pt1 = (x1, y1)
+            pt2 = (x1 + w, y1 + h)
+            (X, Y) = centroid[i]
+
+            # Bounding boxes for each component
+            cv2.rectangle(new_img, pt1, pt2,
+                          (0, 255, 0), 3)
+            cv2.circle(new_img, (int(X),
+                                 int(Y)),
+                       4, (0, 0, 255), -1)
+
+            # Create a new array to show individual component
+            component = np.zeros(gray_img.shape, dtype="uint8")
+            componentMask = (label_ids == i).astype("uint8") * 255
+
+            # Apply the mask using the bitwise operator
+            component = cv2.bitwise_or(component, componentMask)
+            output = cv2.bitwise_or(output, componentMask)
+
+            # Show the final images
+            cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+            cv2.imshow("image", new_img)
+            key = cv2.waitKey(0)
+
+            cv2.namedWindow("Individual Component", cv2.WINDOW_NORMAL)
+            cv2.imshow("Individual Component", component)
+
+            cv2.namedWindow("Filtered Components", cv2.WINDOW_NORMAL)
+            cv2.imshow("Filtered Components", output)
+
+    cv2.destroyAllWindows()
+
+
+
+def test (img: str):
+    image = cv2.imread(img)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+    cv2.namedWindow("thresh", cv2.WINDOW_NORMAL)
+    cv2.imshow("thresh", thresh)
+    key = cv2.waitKey(0)
+
+    contours, hierachy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    max_area = 0
+    c = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > max_area:
+            max_area = area
+            best_cnt = i
+            image = cv2.drawContours(image, contours, c, (0, 255, 0), 3)
+        c += 1
+
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    cv2.imshow("image", image)
+    key = cv2.waitKey(0)
+
+    mask = np.zeros((gray.shape), np.uint8)
+    cv2.drawContours(mask, [best_cnt], 0, 255, -1)
+
+    pic = cv2.imread(img)
+    out = np.zeros_like(gray)
+    out[mask == 255] = gray[mask == 255]
+    ones_part = out[~np.all(out == 0, axis=1)]
+    zero_part = out[np.all(out == 0, axis=1)]
+    ecg= pic[pic.shape[0]-ones_part.shape[0]:, pic.shape[1]-ones_part.shape[1]:]
+    subj_data=pic[:zero_part.shape[0], :zero_part.shape[1]]
+
+    cv2.namedWindow("ecg", cv2.WINDOW_NORMAL)
+    cv2.imshow("ecg", ecg)
+    key = cv2.waitKey(0)
+
+    cv2.namedWindow("subj_data", cv2.WINDOW_NORMAL)
+    cv2.imshow("subj_data", subj_data)
+    key = cv2.waitKey(0)
 
